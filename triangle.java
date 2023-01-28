1df4d4ee07250827a7cea8184f193ba687877616
tusharamohanty08 <121556411+tusharamohanty08@users.noreply.github.com>
2023-01-28 23:13:11 +0530

    Add files via upload

---
+++ triangle.java
@@ -0,0 +1,7 @@
+ class test  
+ {  
+   public static void main (String args[])  
+    {          float b=4,h =13,area ;  
+                         area = ( b*h) / 2 ;  
+                         System.out.println("Area of Triangle is: "+area);  
+    }}
