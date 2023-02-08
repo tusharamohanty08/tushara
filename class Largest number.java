84ab36a9b2d4f5e4928b1e7f4942fedb4e558183
tusharamohanty08 <121556411+tusharamohanty08@users.noreply.github.com>
2023-02-08 22:01:42 +0530

    Add files via upload

---
+++ class Largest number.java
@@ -0,0 +1,18 @@
+class Largest {
+    /**
+     * @param args
+     */
+    public static void main(String[]args) {
+        int n1 = 2, n2 = 3, n3 = 4;
+        if ( n1 >= n2 && n1 >= n3)
+            System.out.println(" n1 is largest number");
+
+            else if ( n2 >= n1 && n2 >= n3)
+            System.out.println("n2 is largest number");
+
+            else
+            System.out.println("n3 is largest number");
+            
+        
+    }
+}
