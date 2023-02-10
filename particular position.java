1e9610200e83b3ab7b63ca46726bc790947a2431
tusharamohanty08 <121556411+tusharamohanty08@users.noreply.github.com>
2023-02-10 18:52:59 +0530

    Add files via upload

---
+++ particular position.java
@@ -0,0 +1,17 @@
+import java.util.Arrays;
+ class Insertelement {
+    public static void main(String args[])
+    {
+        int[] a = {10,20,30,40,50,60,70,80,90,100};
+        int index = 2;
+        int value = 55;
+        System.out.println("Before Insert "+Arrays.toString(a) );
+ 
+        for(int i=a.length-1;i>index;i--)
+        {
+            a[i]=a[i-1];
+        }
+        a[index]=value;
+        System.out.println("After Insert "+Arrays.toString(a) );
+    }
+}
