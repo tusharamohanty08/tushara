fa26f79e0dd091aa14e52bbf60a780fef2fc43ee
tusharamohanty08 <121556411+tusharamohanty08@users.noreply.github.com>
2023-03-04 20:03:22 +0530

    Add files via upload

---
+++ CombinationExample.java
@@ -0,0 +1,28 @@
+import java.util.ArrayList;
+import java.util.List;
+
+class CombinationExample {  
+    static int fact(int number) {  
+       int f = 1;  
+       int j = 1;  
+       while(j <= number) {  
+          f = f * j;  
+          j++;  
+       }  
+       return f;  
+    }  
+    public static void main(String args[]) {  
+       List<Integer> numbers = new ArrayList<Integer>();      
+       numbers.add(9);  
+       numbers.add(12);  
+       numbers.add(19);  
+       numbers.add(61);  
+       numbers.add(19);        
+       int n = numbers.size();  
+       int r = 2;  
+       int result;  
+         
+       result = fact(n) / (fact(r) * fact(n-r));  
+       System.out.println("The combination value for the numbers list is: " + result);  
+    }  
+ }
